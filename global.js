6be452ff3272bfceb9dcb4208d1fc970ad1f0a49
Desmond Vu <150728641+desmondvu@users.noreply.github.com>
2025-01-24 19:13:46 -0800

    actually fixed linking

--- global.js
+++ global.js
@@ -5,10 +5,10 @@ function $$(selector, context = document) {
 }
 
 let pages = [
-    { url: '../index.html', title: 'Home' },
-    { url: '../contact/index.html', title: 'Contact'},
-    { url: '../projects/index.html', title: 'Projects' },
-    { url: '../resume/index.html', title: 'Resume'},
+    { url: 'index.html', title: 'Home' },
+    { url: 'contact/index.html', title: 'Contact'},
+    { url: 'projects/index.html', title: 'Projects' },
+    { url: 'resume/index.html', title: 'Resume'},
     { url: 'https://github.com/desmondvu', title: 'Github'}
   ];
 
@@ -19,25 +19,22 @@ document.body.prepend(nav);
 for (let p of pages) {
     let url = p.url;
     let title = p.title;
-    let a = document.createElement('a');
-    a.href = url;
-    a.textContent = title;
-    nav.append(a);
-    
-    const ARE_WE_HOME = document.documentElement.classList.contains('home');
 
+    const ARE_WE_HOME = document.documentElement.classList.contains('home');
     if (!ARE_WE_HOME && !url.startsWith('http')) {
         url = '../' + url;
     }
     
-    if (a.host === location.host && a.pathname === location.pathname) {
-        a.classList.add('current');
-    }
-    
-    if (a.host !== location.host) {
-        a.target = "_blank";
-    }
-}
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;  
+   
+    a.classList.toggle(
+        'current',
+        a.host === location.host && a.pathname === location.pathname
+    ); 
+    nav.append(a);   
+  }
 
 document.body.insertAdjacentHTML(
     'afterbegin',
