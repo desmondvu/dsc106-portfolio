e2175970798d926d08e970577afcf06f90e8de80
Desmond Vu <150728641+desmondvu@users.noreply.github.com>
2025-01-31 21:15:10 -0800

    quick update

--- global.js
+++ global.js
@@ -1,5 +1,9 @@
 console.log('IT’S ALIVE!');
+
+
 const ARE_WE_HOME = document.documentElement.classList.contains('home');
+const BASE_PATH = '/portfolio';
+
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
@@ -21,7 +25,7 @@ for (let p of pages) {
     let title = p.title;
 
     if (!ARE_WE_HOME && !url.startsWith('http')) {
-        url = '../' + url;
+        url = BASE_PATH + '/' + url;
     }
     
     let a = document.createElement('a');
@@ -61,3 +65,43 @@ select.addEventListener('input', function (event) {
     document.documentElement.style.setProperty('color-scheme', event.target.value);
     localStorage.colorScheme = event.target.value;
 });
+
+export async function fetchJSON(url) {
+  try {
+      // Fetch the JSON file from the given URL
+      const response = await fetch(url);
+      
+      console.log(response);
+
+      if (!response.ok) {
+        throw new Error(`Failed to fetch projects: ${response.statusText}`);
+      }
+
+      const data = await response.json();
+      return data; 
+
+  } catch (error) {
+      console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
+export function renderProjects(projects, containerElement, headingLevel = 'h2') {
+  // Your code will go here
+  if (!(containerElement instanceof HTMLElement)) {
+    throw new Error('containerElement must be a valid DOM element');
+  }
+
+  containerElement.innerHTML = '';
+  
+  for (const project of projects) {
+    const article = document.createElement('article');
+    
+    article.innerHTML = `
+      <h3>${project.title}</h3>
+      <img src="${project.image}" alt="${project.title}">
+      <p>${project.description}</p>
+    `;
+    
+    containerElement.appendChild(article);
+  }
+}
